668871453a47e445d11fd4a7962047da5d53e363
ShogoSaito <saitos917@gmail.com>
2023-03-27 19:23:41 +0900

    コンポーネント化

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,35 +1,47 @@
-import { useState } from 'react';
+import React from 'react';
 
-const TodoList = () => {
+const TodoList = ({ todos, setTodos }) => {
+    const handleRemoveTask = (index) => {
+        const newTodos = [...todos];
+        newTodos.splice(index, 1);
+        setTodos(newTodos);
+    };
 
-    const initialState = [
-        {
-            task: 'Learn vue.js',
-            isCompleted: false
-        },
-        {
-            task: 'Learn React Hook',
-            isCompleted: false
-        },
-        {
-            task: 'Learn Gatsby.js',
-            isCompleted: false
-        },
-    ]
-
-    const [todos, setTodos] = useState(initialState);
+    const handleUpdateTask = (index) => {
+        const newTodos = todos.map((todo, todoIndex) => {
+            if (todoIndex === index) {
+                todo.isCompleted = !todo.isCompleted;
+            }
+            return todo;
+        });
+        setTodos(newTodos);
+    };
 
     return (
-        <div>
-            <h1>ToDo List</h1>
-            Add Task : <input placeholder="Add New Task" />
-            <ul>
-                {todos.map((todo, index) => (
-                    <li key={index}>{todo.task}</li>
-                ))}
-            </ul>
-        </div>
+        <ul>
+            {todos.map((todo, index) => (
+                <li
+                    key={index}
+                    style={{
+                        textDecoration: todo.isCompleted ? 'line-through' : 'none',
+                    }}
+                >
+                    <input
+                        type="checkbox"
+                        checked={todo.isCompleted}
+                        onChange={() => handleUpdateTask(index)}
+                    />
+                    {todo.task}
+                    <span
+                        onClick={() => handleRemoveTask(index)}
+                        style={{ cursor: 'pointer' }}
+                    >
+                        X
+                    </span>
+                </li>
+            ))}
+        </ul>
     );
-}
+};
 
 export default TodoList;
